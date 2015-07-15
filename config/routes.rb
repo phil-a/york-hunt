ba3bd0e158ab369177510b7837e1316498f7be55
phil-a <phil.alekseev@gmail.com>
2015-07-15 16:59:51 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get 'users/new'
+  #get 'users/show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -12,6 +13,8 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  
+  resources :users
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
