8265efe614dab808336a1ef057189eb5ffd1860f
phil-a <phil.alekseev@gmail.com>
2015-07-21 13:17:23 +0000

    Fix User Update Error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
   
   def update
     @user = User.find(params[:id])
-    if @user.update_attributes(user.params)
+    if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
     else
@@ -45,29 +45,30 @@ class UsersController < ApplicationController
     flash[:success] = "User deleted"
     redirect_to users_url
   end
-  
-  private
-   
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-  end
-  
+
   def logged_in_user
     unless logged_in?
     store_location
     flash[:danger] = "Please log in"
     redirect_to login_url
     end  
-  end
-  
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_url) unless current_user?(@user)
   end  
-
-  def admin_user
-    redirect_to(root_url) unless current_user.admin?
-  end
+  
+  
+  private
+   
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                     :password_confirmation)
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end  
+  
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
   
 end
