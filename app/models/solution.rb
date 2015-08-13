79495b56b15a6e02c38260d10dfc0497687eaf3b
phil9 <phil.alekseev@gmail.com>
2015-08-13 22:54:13 +0000

    Add One-to-One Active Record Association between Questions and Solutions

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -1,7 +1,8 @@
 class Solution < ActiveRecord::Base
     #belongs_to :question_id
     #belongs_to :correct_answer_text
-    attr_accessible :question_id, :correct_answer_text
+    belongs_to :question, class_name: 'Rapidfire::Question'
+    #attr_accessible :question_id, :correct_answer_text
     
     
 end
