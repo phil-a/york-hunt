e120c6d2f7bec45112ae27c39c478ab8277a2fe7
phil9 <phil.alekseev@gmail.com>
2015-08-17 20:39:53 +0000

    Add Random Messages to Posts based on Answers

--- app/models/rapidfire/answer_group.rb
+++ app/models/rapidfire/answer_group.rb
@@ -12,11 +12,32 @@ module Rapidfire
         user = User.find_by(id: (self.user_id))
         answers = Rapidfire::Answer.where(answer_group_id: self.id)
         answers_cnt = answers.count - 1
+        correct_cnt = 0
         answers_text = []
         for i in (0..answers_cnt)
-          answers_text << answers[i].answer_text + answers[i].correct?
+          #answers_text << answers[i].answer_text + answers[i].correct?
+          answers_text << answers[i].correct?
+          if answers[i].correct? == "\u2705"
+            correct_cnt = correct_cnt + 1
+          end
         end
-        user.microposts.create!(content: user.name + " posted an answer!" + answers_text.to_s)
+
+        #Create Post
+        if (correct_cnt == Rapidfire::Answer.where(answer_group_id: self.id).count) # correct answers
+          user.microposts.create!(content: user.name + " posted all correct answers! " + random_correct_message + answers_text.to_s)
+        else  #incorrect answers
+          user.microposts.create!(content: user.name + " posted some incorrect answers. " + random_incorrect_message + answers_text.to_s)
+        end
+    end
+
+    def random_incorrect_message
+      incorrect = %w(You\ suck! Go\ back\ and\ try\ again. You\ needs\ some\ learning.)
+      return incorrect[rand(0..incorrect.count-1)]
+    end
+
+    def random_correct_message
+      correct = %w(Finally! You\ rock! Good\ stuff.)
+      return correct[rand(0..correct.count-1)]
     end
     
   end
