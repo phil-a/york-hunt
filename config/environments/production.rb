f63a94f33f80be9501f87929d9bc2b2321cef575
phil9 <phil.alekseev@gmail.com>
2015-08-24 04:40:03 +0000

    Fix Signup production issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://thawing-island-6299.herokuapp.com'
+  host = 'https://polar-reef-8426.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
